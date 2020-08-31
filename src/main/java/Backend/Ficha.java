103ca2d41a59b19edfd07e37cbb91d623e931d9d
OscarDeLeon19 <oscar99deleon19@gmail.com>
2020-08-30 18:24:19 -0600

    Atributos de la clase Ficha

--- src/main/java/Backend/Ficha.java
+++ src/main/java/Backend/Ficha.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Backend;
 
-/**
- *
- * @author oscar19
- */
+
 public class Ficha {
+    
+    String Nombre;
+    String ID;
+    Ficha arriba;
+
+    public Ficha(String Nombre, String ID) {
+        this.Nombre = Nombre;
+        this.ID = ID;
+    }
+    
     
 }
