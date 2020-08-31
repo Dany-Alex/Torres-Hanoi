7d23e80a92cff81a293c24fd0d7cd84e99c63eac
Daniel <dany.phone7@gmail.com>
2020-08-30 22:16:59 -0600

    Se creo la parte logica que resuelve y devuelve una string de los pasos ha ralizar para completar el juego.
    Tambien retorna la cantidad de movientos minimos para completar el juego.

---
+++ src/main/java/Backend/Hanoi.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Backend;
+
+import java.net.ContentHandlerFactory;
+import java.util.Scanner;
+
+/**
+ *
+ * @author goldtux
+ */
+public class Hanoi {
+
+    String mensaje="";
+
+    int contadorMovimentos= 0;
+
+/**
+ * 
+ * @param numeroFichases es la cantidad de ficha que el usuario elige para jugar
+ * @param torreInicio   es la torre donde se encuentran todas las fichas al inicio del juego
+ * @param torreAuxiliar es la torre de apoyo (la que esta en el medio)
+ * @param torreDestino  es la torre donde se desea mover cada ficha para completar el juego
+ */
+    public void resolver(int numeroFichas, int torreInicio, int torreAuxiliar, int torreDestino) {
+
+        if (numeroFichas== 1) {
+            resolver(torreInicio, torreDestino);
+        } else {
+            resolver(numeroFichas - 1, torreInicio, torreDestino, torreAuxiliar);
+            resolver(torreInicio, torreDestino);
+            resolver(numeroFichas - 1, torreAuxiliar, torreInicio, torreDestino);
+        }
+    }
+    
+/**
+ * 
+ * @param torreInicio   es la torre donde se encuentran todas las fichas al inicio del juego
+ * @param torreDestino  es la torre donde se desea mover cada ficha para completar el juego
+ */
+    public void resolver(int torreInicio, int torreDestino) {
+        contadorMovimentos++;
+        String mensajeAux;
+        mensajeAux=" Movimiento #"+ contadorMovimentos+": Torre " + torreInicio + " a Torre " + torreDestino+"\n";
+        mensaje+=mensajeAux;
+ 
+    }
+    /**
+     * 
+     * @return retorna un string donde se concatenan todos pasos de cada movimiento ha ralizar para completar el juego
+     */
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    /**
+     * 
+     * @return retorna la cantidad de movimeintos minimos 
+     */
+    public int getContadorMovimentos() {
+        return contadorMovimentos;
+    }
+    
+}
